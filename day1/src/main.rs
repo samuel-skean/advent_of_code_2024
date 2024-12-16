d7f402071c68062a9a3aa2f0ca6e68c47a35ae51
Samuel Skean <sskean2@uic.edu>
2024-12-15 21:24:27 -0600

    Day 1 part 2 complete!
    Good fun!

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,37 +1,36 @@
 use std::{
-    cmp::Reverse,
-    collections::BinaryHeap,
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 fn main() {
     let input_file = BufReader::new(File::open("input.txt").unwrap());
-    let mut heap1: BinaryHeap<Reverse<u64>> = BinaryHeap::new();
-    let mut heap2: BinaryHeap<Reverse<u64>> = BinaryHeap::new();
+    let mut list1: Vec<u64> = Vec::new();
+    // Map from numbers in the list to the number of their occurences:
+    let mut list2_counter: HashMap<u64, u64> = HashMap::new();
 
     for (line_num, line) in input_file.lines().enumerate() {
         let line = line.expect(&format!("Error reading line: {line_num}"));
         let mut number_strings = line.split_whitespace();
-        heap1.push(Reverse(
-            number_strings
-                .next()
-                .expect(&format!("No first element in line {line_num}"))
-                .parse()
-                .expect(&format!("Couldn't parse second number on line {line_num}")),
-        ));
-        heap2.push(Reverse(
-            number_strings
-                .next()
-                .expect(&format!("No second element in line {line_num}"))
-                .parse()
-                .expect(&format!("Couldn't parse second number on line {line_num}")),
-        ));
+        let list1_num = number_strings
+            .next()
+            .expect(&format!("No first element in line {line_num}"))
+            .parse()
+            .expect(&format!("Couldn't parse second number on line {line_num}"));
+        let list2_num = number_strings
+            .next()
+            .expect(&format!("No second element in line {line_num}"))
+            .parse()
+            .expect(&format!("Couldn't parse second number on line {line_num}"));
+
+        list1.push(list1_num);
+        *list2_counter.entry(list2_num).or_insert(0) += 1;
     }
 
-    let mut sum_of_differences = 0;
-    for (Reverse(n1), Reverse(n2)) in heap1.into_sorted_vec().into_iter().zip(heap2.into_sorted_vec().into_iter()) {
-        sum_of_differences += n1.abs_diff(n2);
+    let mut similarity_score = 0;
+    for list1_num in list1 {
+        similarity_score += list1_num * list2_counter.get(&list1_num).unwrap_or(&0);
     }
-    println!("Sum of differences was: {sum_of_differences}");
+    println!("Similarity Score: {similarity_score}");
 }
